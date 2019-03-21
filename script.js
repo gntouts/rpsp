c019d408def89e49505c3727aef11e121247cfc1
gntouts <ece8441@upnet.gr>
2019-03-21 17:27:31 +0200

    added a username feature

---
+++ script.js
@@ -0,0 +1,6 @@
+var userName = document.getElementById("username").value;
+document.getElementById("output").textContent = userName;
+
+function changeText(element) {
+    document.getElementById('initname').innerHTML = element.value;
+  }
