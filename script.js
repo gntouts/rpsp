8881653d3045f896c065630f128c77765d15efcb
gntouts <ece8441@upnet.gr>
2019-03-22 14:27:24 +0200

    Updated version of random, restore input function

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 var userName = document.getElementById("username").value;
 document.getElementById("output").textContent = userName;
 
+function changeText(element) {
+    document.getElementById('initname').innerHTML = element.value;
+}
+
 function pcChoice(){
     let choice = "";
     let x = Math.random();
